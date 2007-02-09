006a77268f7a1c230655cd95682c45e37ac22958
willem <willem>
2007-02-09 15:29:33 +0000

    Test eof in parsing

--- tests/ast-test.lisp
+++ tests/ast-test.lisp
@@ -1,10 +1,13 @@
-(asdf:operate 'asdf:load-op :clpython)
-
 (defpackage :clpython.test
   (:use :common-lisp :clpython :util.test)
   (:export #:run))
 
 (in-package :clpython.test)
+
+(defun ps (s &optional (incl-module t))
+  (if incl-module
+      (parse-python-string s :incl-module t)
+    (car (parse-python-string s :incl-module nil))))
 
 (defun run (&optional test-compile-load)
   (with-tests (:name :clpython.ast)
@@ -28,61 +31,57 @@
       (test-no-warning (compile-file "../ast/parser.lisp"))
       (test-no-warning (load "../ast/parser.lisp")))
     
-    (flet ((ps (s &optional (incl-module t)) 
-	     (if incl-module
-		 (parse-python-string s :incl-module t)
-	       (car (parse-python-string s :incl-module nil)))))
+    ;; atoms
+    (test '(module-stmt (suite-stmt (42))) (ps "42") :test 'equal)
+    (test '(module-stmt (suite-stmt ("x"))) (ps "'x'") :test 'equal)
       
-      ;; atoms
-      (test '(module-stmt (suite-stmt (42))) (ps "42") :test 'equal)
-      (test '(module-stmt (suite-stmt ("x"))) (ps "'x'") :test 'equal)
+    ;; symbols
+    (test '(assign-stmt 3 ((identifier-expr clpython.ast.user::|y|)))
+	  (ps "y = 3" nil) :test 'equal)
+    (test '(assign-stmt 3 ((identifier-expr clpython.builtin:|len|))) (ps "len = 3" nil) :test 'equal)
       
-      ;; symbols
-      (test '(assign-stmt 3 ((identifier-expr clpython.ast.user::|y|)))
-	    (ps "y = 3" nil) :test 'equal)
-      (test '(assign-stmt 3 ((identifier-expr clpython.builtin:|len|))) (ps "len = 3" nil) :test 'equal)
+    ;; suffix operations
+    (test '(attributeref-expr
+	    (call-expr
+	     (subscription-expr (identifier-expr clpython.ast.user::|x|) 1)
+	     ((2) nil nil nil))
+	    (identifier-expr clpython.ast.user::|a3|))
+	  (ps "x[1](2).a3" nil) :test 'equal)
       
-      ;; suffix operations
-      (test '(attributeref-expr
-	      (call-expr
-	       (subscription-expr (identifier-expr clpython.ast.user::|x|) 1)
-	       ((2) nil nil nil))
-	      (identifier-expr clpython.ast.user::|a3|))
-	    (ps "x[1](2).a3" nil) :test 'equal)
+    ;; call arguments
+    (test '(attributeref-expr
+	    (call-expr (subscription-expr (identifier-expr clpython.ast.user::|x|)
+			1)
+	     (nil (((identifier-expr clpython.builtin:|len|) 2)) nil nil))
+	    (identifier-expr clpython.ast.user::|a3|))
+	  (ps "x[1](len=2).a3" nil) :test 'equal)
       
-      ;; call arguments
-      (test '(attributeref-expr
-	      (call-expr (subscription-expr (identifier-expr clpython.ast.user::|x|)
-			  1)
-	       (nil (((identifier-expr clpython.builtin:|len|) 2)) nil nil))
-	      (identifier-expr clpython.ast.user::|a3|))
-	    (ps "x[1](len=2).a3" nil) :test 'equal)
-      
-      (test '(call-expr 
-	      (identifier-expr clpython.ast.user::|f|)
-	      ((1 2) (((identifier-expr clpython.ast.user::|y|) 3))
-	       (identifier-expr clpython.ast.user::|args|)
-	       (identifier-expr clpython.ast.user::|kw|)))
-	    (ps "f(1,2,y=3,*args,**kw)" nil) :test 'equal)
+    (test '(call-expr 
+	    (identifier-expr clpython.ast.user::|f|)
+	    ((1 2) (((identifier-expr clpython.ast.user::|y|) 3))
+	     (identifier-expr clpython.ast.user::|args|)
+	     (identifier-expr clpython.ast.user::|kw|)))
+	  (ps "f(1,2,y=3,*args,**kw)" nil) :test 'equal)
 
-      ;; function decorators
-      (test '(funcdef-stmt
-	      ;; list of decorators: first foo(bar)
-	      ((call-expr (clpython.ast.user::|foo|) (((identifier-expr clpython.ast.user::|bar|))
-						      nil nil nil))
-	       ;; second deco: zut
-	       (clpython.ast.user::|zut|))
+    ;; function decorators
+    (test '(funcdef-stmt
+	    ;; list of decorators: first foo(bar)
+	    ((call-expr (clpython.ast.user::|foo|) (((identifier-expr clpython.ast.user::|bar|))
+						    nil nil nil))
+	     ;; second deco: zut
+	     (clpython.ast.user::|zut|))
 	      
-	      (identifier-expr clpython.ast.user::|f|)
-	      (nil nil nil nil)
-	      (suite-stmt ((pass-stmt))))
-	    (ps "
+	    (identifier-expr clpython.ast.user::|f|)
+	    (nil nil nil nil)
+	    (suite-stmt ((pass-stmt))))
+	  (ps "
 @foo(bar)
 @zut
 def f(): pass" nil) :test 'equal)
-      
-      ) ;; flet
     
+    ;; Empty string is parsed as module without body.
+    (test nil (ps "" nil))
+
     ;; 'y = yield x' is todo, currently raises SyntaxError.
     (test-error (parse-python-string "y = yield x")
 		:condition-type 'clpython::|SyntaxError|)
@@ -95,8 +94,18 @@ def f(): pass" nil) :test 'equal)
 	    (parse-python-file fname :incl-module nil) :test 'equal)
       (test t (excl.osi:unlink fname)))
     
-    ;; walk.lisp
+    ;; handling eof
+    (flet ((test-eof (want-eof-signal)
+	     (block :body
+	       (let ((*signal-unexpected-eof* want-eof-signal))
+		 (handler-case
+		     (parse-python-string "if 3:")
+		   (unexpected-eof () (return-from :body :eof-signal))
+		   (|SyntaxError|  () (return-from :body :syntax-error)))))))
+      (test :eof-signal   (test-eof t))
+      (test :syntax-error (test-eof nil)))
     
+    ;; walk.lisp
     ;; astpp.lisp
     ;; lispy.lisp
     ))
