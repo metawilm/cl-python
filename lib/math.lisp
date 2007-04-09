990bb91bb789c902cba8e5ad951d4d987927a94d
willem <willem>
2007-04-09 15:37:12 +0000

    Clean up MATH module

--- lib/math.lisp
+++ lib/math.lisp
@@ -1,6 +1,5 @@
 (defpackage :clpython.module.math
-  (:use :clpython :common-lisp))
+  (:use :clpython :common-lisp)
+  (:export #:|pi|))
 
 (in-package :clpython.module.math)
-
-(defconstant pi pi)
