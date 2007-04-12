dac3d9ebdfa22af8693d27f0ecd40e6d0019f473
willem <willem>
2007-04-12 21:34:42 +0000

    Fix accidental typo

--- core/compiler.lisp
+++ core/compiler.lisp
@@ -1,6 +1,6 @@
 ;; -*- package: clpython; readtable: py-ast-user-readtable -*-
 ;;
-o;; This software is Copyright (c) Franz Inc. and Willem Broekema.
+;; This software is Copyright (c) Franz Inc. and Willem Broekema.
 ;; Franz Inc. and Willem Broekema grant you the rights to
 ;; distribute and use this software as governed by the terms
 ;; of the Lisp Lesser GNU Public License
