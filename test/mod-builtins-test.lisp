1abae1913b5e6ccdff6128d9d9a34b0266c3acc9
willem <willem>
2007-09-01 21:38:49 +0000

    Add builtins test

---
+++ test/mod-builtins-test.lisp
@@ -0,0 +1,30 @@
+;; -*- package: clpython.test -*-
+;;
+;; This software is Copyright (c) Franz Inc. and Willem Broekema.
+;; Franz Inc. and Willem Broekema grant you the rights to
+;; distribute and use this software as governed by the terms
+;; of the Lisp Lesser GNU Public License
+;; (http://opensource.franz.com/preamble.html),
+;; known as the LLGPL.
+
+;;;; Python builtins test
+
+(in-package :clpython.test)
+
+(defun run-builtin-test ()
+  (with-subtest (:name "CLPython-Builtins")
+    (dolist (x '(:globals))
+      (test-builtin x))))
+
+(defgeneric test-builtin (kind))
+
+(defmethod test-builtin :around (kind)
+  (with-subtest (:name (format nil "CLPython-Builtin-~A" kind))
+    (let ((*warn-unused-function-vars* nil))
+      (call-next-method))))
+
+(defmethod test-builtin ((x (eql :globals)))
+  (run-no-error "
+g = globals()
+g['x'] = 3
+assert x == 3"))
