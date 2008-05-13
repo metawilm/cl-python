6079a5f9104473baa135f02326f15eb70196d81d
willem <willem>
2008-05-13 20:56:10 +0000

    Fixes in RE, STRING modules after SBCL complaints.

--- lib/re.lisp
+++ lib/re.lisp
@@ -20,43 +20,33 @@
   ())
 
 (def-py-method reg-exp.match (re string &optional start end)
-  (declare (ignorable re string start end))
   (error "todo"))
 
 (def-py-method reg-exp.search (re string &optional start end)
-  (declare (ignorable re string start end))
   (error "todo"))
 
 (def-py-method reg-exp.split (re string &optional (maxsplit 0))
-  (declare (ignorable re string maxsplit))
   (error "todo"))
 
 (def-py-method reg-exp.findall (re string &optional start end)
-  (declare (ignorable re string start end))
   (error "todo"))
 
 (def-py-method reg-exp.finditer (re string &optional start end)
-  (declare (ignorable re string start end))
   (error "todo"))
 
 (def-py-method reg-exp.sub (re repl string  &optional (count 0))
-  (declare (ignorable re repl string count))
   (error "todo"))
 
 (def-py-method reg-exp.subn (re repl string  &optional (count 0))
-  (declare (ignorable re repl string count))
   (error "todo"))
 
 (def-py-method reg-exp.flags :attribute (re)
-  (declare (ignorable re))
   (error "todo"))
 
 (def-py-method reg-exp.groupindex :attribute (re)
-  (declare (ignorable re))
   (error "todo"))
 
 (def-py-method reg-exp.pattern :attribute (re)
-  (declare (ignorable re))
   (error "todo"))
 
 
@@ -64,53 +54,40 @@
   ())
 
 (def-py-method reg-exp-match.expand (rm template)
-  (declare (ignorable rm template))
   (error "todo"))
 
 (def-py-method reg-exp-match.group (rm &rest groups)
-  (declare (ignorable rm groups))
   (error "todo"))
 
 (def-py-method reg-exp-match.groups (rm &optional default)
-  (declare (ignorable rm default))
   (error "todo"))
 
 (def-py-method reg-exp-match.groupdict (rm &optional default)
-  (declare (ignorable rm default))
   (error "todo"))
 
 (def-py-method reg-exp-match.start (rm &optional group)
-  (declare (ignorable rm group))
   (error "todo"))
 
 (def-py-method reg-exp-match.end (rm &optional group)
-  (declare (ignorable rm group))
   (error "todo"))
 
 (def-py-method reg-exp-match.span (rm &optional group)
-  (declare (ignorable rm group))
   (error "todo"))
 
 (def-py-method reg-exp-match.pos :attribute (rm)
-  (declare (ignorable rm))
   (error "todo"))
 
 (def-py-method reg-exp-match.endpos :attribute (rm)
-  (declare (ignorable rm))
   (error "todo"))
 
 (def-py-method reg-exp-match.lastindex :attribute (rm)
-  (declare (ignorable rm))
   (error "todo"))
 
 (def-py-method reg-exp-match.lastgroup :attribute (rm)
-  (declare (ignorable rm))
   (error "todo"))
 
 (def-py-method reg-exp-match.re :attribute (rm)
-  (declare (ignorable rm))
   (error "todo"))
 
 (def-py-method reg-exp-match.string :attribute (rm)
-  (declare (ignorable rm))
   (error "todo"))
--- lib/string.lisp
+++ lib/string.lisp
@@ -2,71 +2,67 @@
   (:use :clpython :common-lisp)
   (:export "ascii_letters" "ascii_lowercase" "ascii_uppercase" "digits"
            "hexdigits" "octdigits" "lowercase" "uppercase" "letters"
-           "punctuation" "whitespace" "printable"))
+           "punctuation" "whitespace" "printable")
+  (:import-from :clpython.package #:defconstant-once))
   
 (in-package :clpython.module.string)
 
-#.(progn
-    (defun char-range (from to)
-      (declare (optimize (speed 3)))
-      (let ((f (lambda (ch) (<= (the fixnum (char-code from))
-                                (the fixnum (char-code ch))
-                                (the fixnum (char-code to))))))
-        (declare (dynamic-extent f))
-        (chars-satisfying f)))
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defun char-range (from to)
+    (declare (optimize (speed 3)))
+    (let ((f (lambda (ch) (<= (the fixnum (char-code from))
+                              (the fixnum (char-code ch))
+                              (the fixnum (char-code to))))))
+      (declare (dynamic-extent f))
+      (chars-satisfying f)))
+  
+  (defun chars-satisfying (pred)
+    (declare (optimize (speed 3)))
+    ;; Allegro does stack allocation if size is a literal number, but not if
+    ;; it is a constant number; therefore #. of char-code-limit. Also,
+    ;; stack-allocated bit vectors can't have an initializer.
+    (let ((bit-arr (make-array #.char-code-limit :element-type 'bit)))
+      (declare (dynamic-extent bit-arr))
+      (do ((i (1- char-code-limit) (1- i)))
+          ((< i 0))
+        (declare (fixnum i))
+        (setf (sbit bit-arr i)
+          (if (funcall pred (code-char i)) 1 0)))
+      (let* ((num-chars (loop for i across bit-arr sum i))
+             (char-arr (make-array num-chars :element-type 'character)))
+        (declare (fixnum num-chars))
+        (loop for i fixnum from 0 below char-code-limit
+            with ci fixnum = 0
+            when (= (sbit bit-arr i) 1)
+            do (setf (schar char-arr ci) (code-char i))
+               (incf ci)
+               (when (= ci num-chars)
+                 (return)))
+        char-arr))))
 
-    (defun chars-satisfying (pred)
-      (declare (optimize (speed 3)))
-      ;; Allegro does stack allocation if size is a literal number, but not if
-      ;; it is a constant number; therefore #. of char-code-limit. Also,
-      ;; stack-allocated bit vectors can't have an initializer.
-      (let ((bit-arr (make-array #.char-code-limit :element-type 'bit)))
-        (declare (dynamic-extent bit-arr))
-        (do ((i (1- char-code-limit) (1- i)))
-            ((< i 0))
-          (declare (fixnum i))
-          (setf (sbit bit-arr i)
-            (if (funcall pred (code-char i)) 1 0)))
-        (let* ((num-chars (loop for i across bit-arr sum i))
-               (char-arr (make-array num-chars :element-type 'character)))
-          (declare (fixnum num-chars))
-          (loop for i fixnum from 0 below char-code-limit
-              with ci fixnum = 0
-              when (= (sbit bit-arr i) 1)
-              do (setf (schar char-arr ci) (code-char i))
-                 (incf ci)
-                 (when (= ci num-chars)
-                   (return)))
-          char-arr)))
-
-    (compile 'char-range)
-    (compile 'chars-satisfying)
-    
-    nil)
-
-(defconstant |ascii_lowercase| #.(char-range #\a #\z))
-(defconstant |ascii_uppercase| #.(char-range #\A #\Z))
-(defconstant |ascii_letters| (concatenate 'string
-                               |ascii_lowercase|
-                               |ascii_uppercase|))
+(defconstant-once |ascii_lowercase| #.(char-range #\a #\z))
+(defconstant-once |ascii_uppercase| #.(char-range #\A #\Z))
+(defconstant-once |ascii_letters| (concatenate 'string
+                                    |ascii_lowercase|
+                                    |ascii_uppercase|))
 (set-impl-status '(|ascii_letters| |ascii_lowercase| |ascii_uppercase|) t)
 
-(defconstant |digits| #.(char-range #\0 #\9))
-(defconstant |hexdigits| #.(concatenate 'string
-                             (char-range #\0 #\9)
-                             (char-range #\a #\f)
-                             (char-range #\A #\F)))
-(defconstant |octdigits| #.(char-range #\0 #\7))
+(defconstant-once |digits| #.(char-range #\0 #\9))
+(defconstant-once |hexdigits| #.(concatenate 'string
+                                  (char-range #\0 #\9)
+                                  (char-range #\a #\f)
+                                  (char-range #\A #\F)))
+(defconstant-once |octdigits| #.(char-range #\0 #\7))
 (set-impl-status '(|digits| |hexdigits| |octdigits|) t)
 
-(defconstant |lowercase| #.(chars-satisfying #'lower-case-p))
-(defconstant |uppercase| #.(chars-satisfying #'upper-case-p))
-(defconstant |letters| (concatenate 'string |lowercase| |uppercase|))
+(defconstant-once |lowercase| #.(chars-satisfying #'lower-case-p))
+(defconstant-once |uppercase| #.(chars-satisfying #'upper-case-p))
+(defconstant-once |letters| (concatenate 'string |lowercase| |uppercase|))
 (set-impl-status '(|lowercase| |uppercase| |letters|) t)
 
-(defconstant |punctuation| "'!\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'")
-(defconstant |whitespace| #.(coerce '(#\Space #\Tab #\Newline #\Return #\Page) 'string))
-(defconstant |printable| (concatenate 'string |digits| |letters| |punctuation| |whitespace|))
+(defconstant-once |punctuation| "'!\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'")
+(defconstant-once |whitespace| #.(coerce '(#\Space #\Tab #\Newline #\Return #\Page) 'string))
+(defconstant-once |printable| (concatenate 'string |digits| |letters| |punctuation| |whitespace|))
 
 (set-impl-status '(|punctuation| |printable|)
                  :incomplete "Value of punctuation copied from CPython; need to verify")
