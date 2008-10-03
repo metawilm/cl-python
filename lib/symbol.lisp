12f6d2f1dd250ced610ca8460655f32b4607e813
willem <willem>
2008-10-03 19:30:04 +0000

    Add symbol.lisp

---
+++ lib/symbol.lisp
@@ -0,0 +1,13 @@
+;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: CLPYTHON.MODULE.GC -*-
+;;
+;; This software is Copyright (c) Franz Inc. and Willem Broekema.
+;; Franz Inc. and Willem Broekema grant you the rights to
+;; distribute and use this software as governed by the terms
+;; of the Lisp Lesser GNU Public License
+;; (http://opensource.franz.com/preamble.html),
+;; known as the LLGPL.
+
+;;; CPython Parse tree constants
+
+(in-package :clpython.module.symbol)
+
