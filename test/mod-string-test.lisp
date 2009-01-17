fdf8d55e95d2c492b993c7680e3adbf8c351c591
willem <willem>
2009-01-17 01:49:28 +0000

    Add string test

---
+++ test/mod-string-test.lisp
@@ -0,0 +1,16 @@
+;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: CLPYTHON.TEST -*-
+;;
+;; This software is Copyright (c) Franz Inc. and Willem Broekema.
+;; Franz Inc. and Willem Broekema grant you the rights to
+;; distribute and use this software as governed by the terms
+;; of the Lisp Lesser GNU Public License
+;; (http://opensource.franz.com/preamble.html),
+;; known as the LLGPL.
+
+(in-package :clpython.test)
+
+(defun run-mod-string-test ()
+  (run-no-error "
+import string
+t = string.maketrans('ab', 'xy')
+string.translate('abcdxyz', t, 'd') == 'xycxyz'"))
