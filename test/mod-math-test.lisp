d5b2bc47dbc3a5e872e9f8f3e18f8d42d942acfd
willem <willem>
2009-01-17 10:57:22 +0000

    Add missing math test file

---
+++ test/mod-math-test.lisp
@@ -0,0 +1,14 @@
+;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: CLPYTHON.TEST -*-
+;;
+;; This software is Copyright (c) Franz Inc. and Willem Broekema.
+;; Franz Inc. and Willem Broekema grant you the rights to
+;; distribute and use this software as governed by the terms
+;; of the Lisp Lesser GNU Public License
+;; (http://opensource.franz.com/preamble.html),
+;; known as the LLGPL.
+
+(in-package :clpython.test)
+
+(defun run-mod-math-test ()
+  (run-no-error "import math; assert 3.14 < math.pi < 3.15")
+  (run-no-error "import math; assert 2.71 < math.e < 2.72"))
