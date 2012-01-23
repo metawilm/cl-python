252acbc3077ae24406ca1c800b0087e038c7d5dc
Willem Broekema <metawilm@gmail.com>
2012-01-23 10:40:59 +0100

    Add module _functools

---
+++ lib/_functools.lisp
@@ -0,0 +1,24 @@
+;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: CLPYTHON.MODULE._FUNCTOOLS -*-
+;;
+;; This software is Copyright (c) Franz Inc. and Willem Broekema.
+;; Franz Inc. and Willem Broekema grant you the rights to
+;; distribute and use this software as governed by the terms
+;; of the Lisp Lesser GNU Public License
+;; (http://opensource.franz.com/preamble.html),
+;; known as the LLGPL.
+
+(in-package :clpython.module._functools)
+
+(defun |partial| (func &rest pos-kw-args-1)
+  (let* ((pos-args-1 (loop while pos-kw-args-1
+                         until (keywordp (car pos-kw-args-1))
+                         collect (pop pos-kw-args-1)))
+         (kw-args-1 pos-kw-args-1))
+    (clpython.util:named-function :partial
+      (lambda (&rest pos-kw-args-2)
+        (let* ((pos-args-2 (loop while pos-kw-args-2
+                               until (keywordp (car pos-kw-args-2))
+                               collect (pop pos-kw-args-2)))
+               (kw-args-2 pos-kw-args-2))
+          (let ((total-pos-args (append pos-args-1 pos-args-2 kw-args-1 kw-args-2)))
+            (apply #'py-call func total-pos-args)))))))
